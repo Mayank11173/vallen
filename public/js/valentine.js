4fa48403fa663d89f1100a2ebafc1f77a5c91726
Mayank kandpal <kandpalmayank3@gmail.com>
2026-03-26 18:18:50 +0530

    Delete public directory
    Delete message

--- public/js/valentine.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  const btn = document.getElementById("valentineBurst");
-  btn?.addEventListener("click", () => {
-    window.ValentineFX?.burst?.({ count: 26 });
-    window.ValentineUI?.toast?.("My Vallentine 💗", { kind: "good", title: "For Gunjan" });
-  });
-})();
